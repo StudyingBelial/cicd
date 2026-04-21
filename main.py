97dc409b43f837c0a378ca0d1c8d144b8146acbf
StudyingBelial <baarya24@tbc.edu.np>
2026-04-21 11:04:32 +0545

    Added main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+def Add(a, b):
+    return a + b
+
+def Subtract(a, b):
+    return a - b
+
+def CallHello():
+    print("Hello world from Github Actions Tutorial")
+    
+if __name__ == "__main__":
+    CallHello()
