e86f5dc3f91fbd2e03f94141f2867976bb092258
StudyingBelial <baarya24@tbc.edu.np>
2026-04-21 12:01:22 +0545

    Added cide as well a requirements

--- main-test.py
+++ main-test.py
@@ -1,14 +1,14 @@
 from main import Add, Subtract
 
-def TestAdd():
+def test_Add():
     assert Add(2, 3) == 5
     assert Add(5, 5) == 10
     print("Add Function works correctly")
     
-def TestSubtract():
+def test_Subtract():
     assert Subtract(5, 2) == 3
     print("Subtract Function works correctly")
     
 if __name__ == "__main__":
-    TestAdd()
-    TestSubtract()
+    test_Add()
+    test_Subtract()
