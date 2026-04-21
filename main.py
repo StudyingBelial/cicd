1904e8b46b18cbedc3281a7823eb7603a4da6177
StudyingBelial <baarya24@tbc.edu.np>
2026-04-21 13:14:32 +0545

    Changed the hello message

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def Subtract(a, b):
     return a - b
 
 def CallHello():
-    print("Hello world from Github Actions Tutorial")
+    return("Hello world from Github Actions Tutorial")
     
 if __name__ == "__main__":
     CallHello()
